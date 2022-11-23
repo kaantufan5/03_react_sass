e5bb52caf3653dcd5b8d84e371687de840a64118
Kaan Tufan <46603841+kaantufan5@users.noreply.github.com>
2022-11-23 23:25:08 +0300

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import cardStyle from '../scss/card.module.scss';
+
+const Card = ({ data }) => {
+  return (
+    <div className={cardStyle["container"]}>
+        {data.map((item) => {
+          const { id, name, job, comment, img} = item;
+          return (
+            <div className={cardStyle['card']} key={id}>
+              <h1>{name}</h1>
+              <h2>{job}</h2>
+              <p>{comment}</p>
+              <img src={img} alt={name} />
+              <div className={cardStyle['btn-container']}>
+                <button className={cardStyle['btn-container--small']}>Small</button>
+                <button className={cardStyle['btn-container--large']}>Large</button>
+              </div>
+            </div>
+          )
+        })}
+    </div>
+  );
+};
+
+export default Card;
